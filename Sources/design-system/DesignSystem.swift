de150a02cbb25d68889f8a6920e0dbf6fd84bc4a
Nick Farrant <nfa@trustpilot.com>
2022-01-17 16:26:53 +0000

    Use app's bundle for resources

--- Sources/design-system/DesignSystem.swift
+++ Sources/design-system/DesignSystem.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 public final class DesignSystem {
-  public static let shared = DesignSystem(model: Bundle.main.makeDesignSystemModelFromFile())
+  private var appBundle: Bundle?
+  internal var model: DesignSystemModel!
 
-  internal let model: DesignSystemModel
-  init(model: DesignSystemModel) {
-    self.model = model
+  public static let shared = DesignSystem()
+
+  public func setAppBundle(_ bundle: Bundle) {
+    appBundle = bundle
+    makeDesignSystemModel()
+  }
+
+  private func makeDesignSystemModel() {
+    guard let appBundle = appBundle else {
+      fatalError("App bundle not set. Please call DesignSystem.shared.setAppBundle(Bundle.module).")
+    }
+    
+    guard let path = appBundle.path(forResource: "designsystem", ofType: "json") else {
+      fatalError("Could not get designsystem.json file from main bundle")
+    }
+
+    do {
+      let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+//        let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+      let decoder = JSONDecoder()
+      let json = try decoder.decode(DesignSystemModel.self, from: data)
+      model = json
+    } catch {
+      print(error.localizedDescription)
+      print(String(describing: error))
+      fatalError("Could not build model")
+    }
   }
 }
