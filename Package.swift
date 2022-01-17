b47a78166f39656e8b57b19ff6de86361c55099f
Nick Farrant <nfa@trustpilot.com>
2022-01-17 15:56:56 +0000

    Add json file to target resources

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "design-system",
-            dependencies: []),
+            resources: [
+              .process("Resources/designsystem.json")
+            ]),
         .testTarget(
             name: "design-systemTests",
             dependencies: ["design-system"]),
